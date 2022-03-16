33627c604868c311038ba624e820f3d3729ad98a
Benny Powers <web@bennypowers.com>
2022-03-16 09:08:21 -0400

    chore: fix ci

--- tests/mininit.lua
+++ tests/mininit.lua
@@ -1,28 +1,19 @@
--- Need the absolute path as when doing the testing we will issue things like `tcd` to change directory
--- to where our temporary filesystem lives
-vim.opt.rtp = {
-  vim.fn.fnamemodify(vim.trim(vim.fn.system("git rev-parse --show-toplevel")), ":p"),
-  vim.env.VIMRUNTIME,
-  vim.fn.expand('./vendor/plenary.nvim'),
-  vim.fn.expand('./vendor/nvim-treesitter'),
-  vim.fn.expand('./vendor/nui.nvim'),
-}
-
-vim.opt.swapfile = false
-
-vim.cmd([[
-  filetype on
-  runtime plugin/regexplainer.vim
-  runtime vendor/plenary.nvim
-  runtime vendor/nui.nvim
-  runtime vendor/nvim-treesitter
-]])
-
-require'nvim-treesitter.configs'.setup {}
-
 -- For debugging
 function P(...)
   print(unpack(vim.tbl_map(vim.inspect, { ... })))
 end
 
+vim.cmd([[
+  set noswapfile
+  set rtp+=./vendor/plenary.nvim
+  set rtp+=./vendor/nvim-treesitter
+  set rtp+=./vendor/nui.nvim
+  filetype on
+  packadd plenary.nvim
+  packadd nui.nvim
+  packadd nvim-treesitter
+  runtime plugin/regexplainer.vim
+]])
+
+require'nvim-treesitter.configs'.setup {}
 
